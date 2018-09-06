cf474d4ded98b48f2fb4dc6eb781fee73420c84d
NihalArman <asmodeusnihal@gmail.com>
2018-09-06 08:08:54 +0600

    first commit

---
+++ resources/views/pages/index.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>welcome to laravel</h1>
+    <p>this is the laravel application from the "Laravel From Scratch" Youtube Series</p>
+@endsection
+
+
